d56502d72514102c8e67d949f830d9be7afee0a6
MariamiMesablishili <mesablishvilimariami1@gmail.com>
2024-03-07 23:23:01 +0000

    homework

---
+++ script.js
@@ -0,0 +1,49 @@
+/*გამოაცხადეთ ორი ცვლადი a,b მიანიჭეთ მნიშვნელობები 23 და 4
+დაბეჭდეთ (console.log) a და b რიცხვების ჯამი
+დავეჭდეთ a და b რიცხვების ნამრავლი */
+let a = 24, b= 4
+k = a+b
+console.log (k)
+d = a*b
+console.log (d)
+ /* გამოახცადეთ ახალი ობიექტის(object) 
+ ტიპის ცვლადი  person თქვენი 
+ მონაცემებით ( first name, last name, age, address(რომელიც შეიცავს: city, country),
+  phone no, email, friends (სახელების სია),  
+  */
+
+  let person= {
+    "first name": "mariami",
+    "surname": "mesablishvili",
+    "age": 33,
+    "address": ["norwich", "the uk"],
+    "phone no": 123,
+    "email": "mesablishvilimariami1@gmail.com", 
+    "friends": ["nino", "nia", "teko"]
+ }
+  console.log (person)
+
+  /* დაბეჭდეთ person ობიექტის სახელი და მეორე მეგობრის სახელი */
+  console.log (person["first name"])
+  let friends = ["nino", "nia", "teko"]
+console.log ( friends[1] )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
